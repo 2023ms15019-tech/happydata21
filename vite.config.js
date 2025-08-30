d4e2607126531757e2d1f1b602232f2cc8132bf3
krupa950 <kumarkrupa96@gmail.com>
2025-08-30 15:59:41 +0530

    Setup GitHub Pages deploy1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/happydata21/",  // 👈 use your repository name here
+});
